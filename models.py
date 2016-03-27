f1b441b46f901c52bda19e5a56f32f1af2248413
Penina Wanjiru <penina.wanjiru@andela.com>
2016-03-27 20:14:20 +0300

    initial commit

---
+++ models.py
@@ -0,0 +1,15 @@
+import datetime
+
+from peewee import *
+
+DATABASE = SqliteDatabase('bucket.db')
+
+
+class User(Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
