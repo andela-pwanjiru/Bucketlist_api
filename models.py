bc45aff516630bc1a5106b36596fabbc333460c6
Penina Wanjiru <penina.wanjiru@andela.com>
2016-04-17 15:27:38 +0300

    created models

--- models.py
+++ models.py
@@ -1,15 +1,52 @@
-import datetime
+import os
+from flask.ext.login import UserMixin
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
-from peewee import *
-
-DATABASE = SqliteDatabase('bucket.db')
+from api import db
 
 
-class User(Model):
-    username = CharField(unique=True)
-    email = CharField(unique=True)
-    password = CharField(max_length=100)
-    joined_at = DateTimeField(default=datetime.datetime.now)
+class Bucketlist(db.Model):
+    """Stores Bucketlists."""
 
-    class Meta:
-        database = DATABASE
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), unique=True)
+    date_created = db.Column(db.DateTime)
+    date_modified = db.Column(db.DateTime)
+    bl_items = db.relationship('Item')
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class Item(db.Model):
+    """Stores Bucketlist Items"""
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    date_created = db.Column(db.DateTime)
+    date_modified = db.Column(db.DateTime)
+    done = db.Column(db.Boolean, default=False)
+    bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlist.id'))
+
+
+class User(db.Model, UserMixin):
+    """Stores users"""
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(128))
+    bucketlists = db.relationship('Bucketlist')
+
+    def hash_password(self, password):
+        """Hashes the password """
+        self.password = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        """Verify a password against an existing hash"""
+        return pwd_context.verify(password, self.password)
+
+    def generate_auth_token(self):
+        """Generates authentication token."""
+        serial = Serializer(os.environ.get('SECRET'), expires_in=1800)
+        data = [str(self.id), self.password]
+        return serial.dumps(data)
