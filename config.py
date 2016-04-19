a0b9b4ba38b5bd73e97b8afb697e2800ddf5e216
Penina Wanjiru <penina.wanjiru@andela.com>
2016-04-20 00:09:01 +0300

    added tests

--- config.py
+++ config.py
@@ -27,6 +27,6 @@ class TestingConfig(Config):
     """Testing configurations."""
     TESTING = True
     if os.getenv('TRAVIS_BUILD', None):
-        SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Andela2015@localhost/tests'
+        SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_URL')
     else:
-        SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Andela2015@localhost/tests'
+        SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_URL')
