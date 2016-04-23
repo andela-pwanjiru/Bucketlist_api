60a451ed4a8a8617cec1a6c3bca83b60bc318ff5
Penina Wanjiru <penina.wanjiru@andela.com>
2016-04-23 23:42:36 +0300

    removed unneccesary code

--- config.py
+++ config.py
@@ -9,11 +9,8 @@ class Config(object):
     """Default configurations."""
     DEBUG = False
     TESTING = False
-    CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET')
-    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-    REMEMBER_COOKIE_DURATION = 600
-    TRAP_BAD_REQUEST_ERRORS = True
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository') 
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
@@ -21,6 +18,7 @@ class DevelopmentConfig(Config):
     """Config for Development."""
     DEVELOPMENT = True
     DEBUG = True
+    TRAP_BAD_REQUEST_ERRORS = True
 
 
 class TestingConfig(Config):
